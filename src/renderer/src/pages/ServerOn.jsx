8bdeb5ee1ca3ef42d42f41fbfb6e38f29ae6ad58
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-12-23 19:11:54 +0530

    Update

--- src/renderer/src/pages/ServerOn.jsx
+++ src/renderer/src/pages/ServerOn.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import { IoIosHome } from "react-icons/io";
+import { Link } from 'react-router-dom';
 const ServerOn = () => {
   return (
     <div className='bg-[#24292e] min-h-screen'>
         <div className="mx-32 text-white py-20">
-            
-            <h1 className="">Configure Backend</h1>
+            <Link to={'/'}>
+                <div className="flex">
+                    <IoIosHome className='w-6 h-auto'/>
+                    <p className="text-sm pt-1">Back to Welcome</p>
+                </div>
+            </Link>
+            <h1 className="mt-4">Configure Backend</h1>
         </div>
     </div>
   )
